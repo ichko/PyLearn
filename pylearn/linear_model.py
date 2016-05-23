11612a2190a34cf1b162c0cb48ec12a1ac1d348e
Iliya Zhechev <i4ko95@gmail.com>
2016-05-23 22:34:56 +0300

    Logistic regression

--- pylearn/linear_model.py
+++ pylearn/linear_model.py
@@ -1,3 +1,5 @@
+from math import exp
+import numpy as np
 from .preprocess import InputData, InitialParameters
 from .cost import rss
 from .trainable_model import TrainableModel
@@ -14,14 +16,17 @@ class LinearRegression(TrainableModel):
         theta = InitialParameters.ones(len(X[0]))
         return self.train(derivative, theta)
 
-    def train(self, derivative, theta):
-        last_derivative = derivative(theta)
-        iteration = self.max_iterations
-        while iteration and abs(sum(last_derivative)) > self.train_threshold:
-            last_derivative = derivative(theta)
-            theta = theta - self.learning_rate * last_derivative
-            iteration -= 1
 
-        self.predict = lambda inp: sum(x * t for x, t in zip([1] + inp, theta))
+class LogisticRegression(TrainableModel):
 
-        return theta
+    def sigmoid(x):
+        return np.vectorize(lambda x: 1 / (1 + exp(-x)))
+
+    def hypothesis(self, X, theta):
+        return self.sigmoid(X.dot(theta))
+
+    def fit(self, X, y):
+        X, y = InputData.normalize(X, y)
+        cost, derivative = rss(X, y, self.hypothesis, self.regularization_term)
+        theta = InitialParameters.ones(len(X[0]))
+        return self.train(derivative, theta)
--- pylearn/trainable_model.py
+++ pylearn/trainable_model.py
@@ -7,5 +7,14 @@ class TrainableModel:
         self.train_threshold = 0.0001
         self.predict = lambda _: 1
 
-    def train(self):
-        raise NotImplementedError()
+    def train(self, derivative, theta):
+        last_derivative = derivative(theta)
+        iteration = self.max_iterations
+        while iteration and abs(sum(last_derivative)) > self.train_threshold:
+            theta = theta - self.learning_rate * last_derivative
+            last_derivative = derivative(theta)
+            iteration -= 1
+
+        self.predict = lambda inp: sum(x * t for x, t in zip([1] + inp, theta))
+
+        return theta
